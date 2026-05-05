2a33792c1d4103aa1ffaa08d936a8ae49e35f21c
TomiC <tomascure24@gmail.com>
2026-05-05 18:17:07 -0300

    Probando openweather API

--- js/index.js
+++ js/index.js
@@ -1,2 +1,25 @@
 const apiKey = "ee2bbf259064f981ca49b2daa8440fc9";
 //ESPERAR UNAS HORAS A QUE FUNCIONE 
+let loc = "rosario";
+const api = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${apiKey}&units=metric`;
+
+function apiFetch(){
+    fetch(api)
+    .then((res) => res.json())
+    .then((data) =>{
+        console.log(data);
+    
+        let temp = data.main.temp;
+        let icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        let weatherType = data.weather[0].description;
+        let humidity = data.main.humidity;
+        let windspeed = data.wind.speed;
+        let cityName = data.name;
+        let countryName = data.sys.country;
+
+        console.log(cityName, countryName, temp, icon, weatherType, humidity, windspeed);
+
+    });
+    }
+
+apiFetch();
